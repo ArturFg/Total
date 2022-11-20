6628a66c8340f398530b4039700a5992b0127fa7
Артур <110016503+ArturFg@users.noreply.github.com>
2022-11-20 20:49:41 +0300

    Проверка 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!"); 
+ 
+ 
+ 
+Console.Write("Введите число, что бы зафиксировать размер массива: "); 
+int length = Convert.ToInt32(Console.ReadLine());  
+ 
+ 
+ 
+int[] arrays = new int[length]; 
+for (int i = 0; i < length; i++) 
+{ 
+    arrays[i] = Convert.ToInt32(Console.ReadLine());  
+} 
+ 
+int index = 0; 
+Console.Write($"[{arrays[index]}, "); 
+for (int i = 1; i < arrays.Length - 1; i++) 
+{ 
+    Console.Write($"{arrays[i]}, "); 
+} 
+Console.Write($"{arrays[arrays.Length - 1]}]");
