5e19aa47d5c3ee84b47c646fb428a5f9bc2a0090
Артур <110016503+ArturFg@users.noreply.github.com>
2022-11-22 20:36:00 +0300

    Добавил вывод со всеми строками и далее вывод с нужными строками

--- Program.cs
+++ Program.cs
@@ -1,41 +1,33 @@
 ﻿Console.WriteLine("Hello, World!"); 
  
 string[] arr = {"Fhneh", "Vi", "Trg", "Yiop"};
+
+int index = 0;
+Console.Write($"[ {arr[index]}, ");
+for (int i = 1; i < arr.Length - 1; i++)
+{
+    Console.Write($"{arr[i]}, ");
+}
+Console.WriteLine($"{arr[arr.Length - 1]}]");
+
 for (int i = 0; i <= 3; i++)
 {
-    int a = arr[i].Length; 
-    Console.WriteLine(a); 
-}
-
-/*Console.Write("Введите число, что бы зафиксировать размер массива: "); 
-int length = Convert.ToInt32(Console.ReadLine());  
- 
-int[] arrays = new int[length]; 
-for (int i = 0; i < length; i++) 
-{ 
-    arrays[i] = Convert.ToInt32(Console.ReadLine());  
-} 
-
-for (int i = 0; i < length; i++)
-{
-    int[] array = new int[1];
-    array[0] = arrays[i];
-    int size = 1;
-    while(array[0] >= 9)
+    if(i == 0)
     {
-        array[0] /= 10;
-        size++;
+        int a = arr[i].Length;
+        if(a <= 3) Console.Write($"[ {arr[i]}, ");
+        else Console.Write("[ ");
     }
-    if(size > 3) arrays[i] = 0;
+    if(i != 3) 
+    {    
+        int b = arr[i].Length;
+        if(b <= 3) Console.Write($"{arr[i]}, ");
+        else Console.Write("");
+    }
+    if(i == 3)
+    {
+        int b = arr[i].Length;
+        if(b <= 3) Console.Write($"{arr[arr.Length - 1]}]");
+        else Console.WriteLine("]");
+    }
 }
-
-int index = 0; 
-if(arrays[index] > 0) Console.Write($"[{arrays[index]}, "); 
-else Console.Write($"[");
-for (int i = 1; i < arrays.Length - 1; i++) 
-{ 
-    if(arrays[i] > 0) Console.Write($"{arrays[i]}, "); 
-    else Console.Write($"");
-} 
-if(arrays[arrays.Length - 1] > 0) Console.Write($"{arrays[arrays.Length - 1]}]");
-else Console.Write($"]");*/
